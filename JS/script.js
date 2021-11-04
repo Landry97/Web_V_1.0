e5d486467a000c87956c22be775495f467c0dbb3
Landry97 <92038899+Landry97@users.noreply.github.com>
2021-11-04 17:43:33 +0100

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
 
-var info = document.getElementById('project-info1')
+var info = document.getElementsByClassName('project-info1')
 var mouseTarget = document.getElementById('mouseTarget')
 var main = document.getElementsByTagName("main")
 var header = document.getElementsByTagName("header")
