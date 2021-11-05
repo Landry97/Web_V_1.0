d5f1b748e939b6f757f96ba2f024a766e556c577
Landry97 <92038899+Landry97@users.noreply.github.com>
2021-11-05 22:18:28 +0100

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -4,7 +4,6 @@ var info2 = document.getElementById('project-info2')
 var info3 = document.getElementById('project-info3')
 var info4 = document.getElementById('project-info4')
 var info5 = document.getElementById('project-info5')
-
 mouseTarget.addEventListener('mouseenter', function(){ 
  info1.style.opacity = "1";
  info2.style.opacity = "1";
@@ -16,9 +15,12 @@ mouseTarget.addEventListener('mouseenter', function(){
  document.getElementById("pictureid3").style.border = "1px solid black";
  document.getElementById("pictureid4").style.border = "1px solid black";
  document.getElementById("pictureid5").style.border = "1px solid black";
+ document.getElementById("bodyid").style.background = "red";
+ document.getElementById("bodyid").style.color = "white";
 })
-
 mouseTarget.addEventListener('mouseleave', function(){
+ document.getElementById("bodyid").style.background = "";
+ document.getElementById("bodyid").style.color = "";
  document.getElementById("pictureid1").style.border = "";
  document.getElementById("pictureid2").style.border = "";
  document.getElementById("pictureid3").style.border = "";
