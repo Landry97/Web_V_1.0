a8930cf45c170e5f98836dd2a8a72390e67b462b
Landry97 <92038899+Landry97@users.noreply.github.com>
2021-11-05 12:38:02 +0100

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -9,7 +9,7 @@ var header = document.getElementsByTagName("header")
 var border = document.getElementsByTagName("figure")
 
 mouseTarget.addEventListener('mouseenter', function(){
- border.style.border = "1px";
+ border.style.borderwidth: "3px";
  border.style.border =  "solid black";
  info1.style.opacity = "1";
  info2.style.opacity = "1";
