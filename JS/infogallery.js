6e6f1a165ecfecad679ab974613774a8ff4808be
Landry97 <92038899+Landry97@users.noreply.github.com>
2021-11-07 02:41:25 +0100

    Update infogallery.js

--- JS/infogallery.js
+++ JS/infogallery.js
@@ -1,7 +1,9 @@
 const infogallery = document.getElementById('infogalleryid');
-const infid = document.getElementById('INFGLLRY')
+const infid = document.getElementById('INFGLLRY');
+const header = document.getElementById('headerid');
 
 infogallery.addEventListener('click', () => {
       infogallery.classList.toggle('show');
       infid.classList.toggle('show');
+      header.classList.toggle('show');
 }); 
