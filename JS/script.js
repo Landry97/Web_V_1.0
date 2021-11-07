23f0ce985f1fa2752f0b89da791964121348a86f
Landry97 <92038899+Landry97@users.noreply.github.com>
2021-11-07 01:00:07 +0100

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -27,6 +27,14 @@ menu.addEventListener('click', () => {
       menupoint.classList.toggle('show');
 }); 
 
+const infogallery = document.getElementById('infogalleryid');
+const infid = document.getElementById('INFGLLRY')
+
+infogallery.addEventListener('click', () => {
+      infogallery.classList.toggle('show');
+      infid.classList.toggle('show');
+}); 
+
 
 //TO TOP BUTTON (working buttom flex right corner start 15pixel)_________________________
 //[Get the button:]
