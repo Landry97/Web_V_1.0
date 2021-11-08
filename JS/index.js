65594a9d1c9544d57f8c4dbb350ac3cfc82b4cad
Landry97 <92038899+Landry97@users.noreply.github.com>
2021-11-08 14:14:12 +0100

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -10,7 +10,7 @@ mouseTarget.addEventListener('mouseenter', function(){
  info3.style.opacity = "1";
  info4.style.opacity = "1";
  info5.style.opacity = "1";
- document.getElementById("bodyid").style.background = "black";
+ document.getElementById("bodyid").style.background = "red";
  document.getElementById("headerindex").style.color = "white";
 })
 mouseTarget.addEventListener('mouseleave', function(){
