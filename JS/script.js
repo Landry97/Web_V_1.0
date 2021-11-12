fcfcddcf1fc81f9c903081e2bbd607819876d745
Landry97 <92038899+Landry97@users.noreply.github.com>
2021-11-12 18:57:59 +0100

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var lazyloadImages = document.querySelectorAll("img.lazy");    
+  var lazyloadImages = document.querySelectorAll("img.image-slide");    
   var lazyloadThrottleTimeout;
   
   function lazyload () {
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
         lazyloadImages.forEach(function(img) {
             if(img.offsetTop < (window.innerHeight + scrollTop)) {
               img.src = img.dataset.src;
-              img.classList.remove('lazy');
+              img.classList.remove('image-slide');
             }
         });
 function scale(x) {
