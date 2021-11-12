97ee3317d3b5c78bd18f97267287aef0fe34fb15
Landry97 <92038899+Landry97@users.noreply.github.com>
2021-11-12 18:56:05 +0100

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,20 @@
-var observer = new IntersectionObserver(
-    (entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.intersectionRatio > 0.0) {
-                picture = entry.target;
-                if (!picture.hasAttribute('src')) {
-                    alert('will load the image!!!');
-                    img.setAttribute('src', img.dataset.src);
-                }
+document.addEventListener("DOMContentLoaded", function() {
+  var lazyloadImages = document.querySelectorAll("img.lazy");    
+  var lazyloadThrottleTimeout;
+  
+  function lazyload () {
+    if(lazyloadThrottleTimeout) {
+      clearTimeout(lazyloadThrottleTimeout);
+    }    
+    
+    lazyloadThrottleTimeout = setTimeout(function() {
+        var scrollTop = window.pageYOffset;
+        lazyloadImages.forEach(function(img) {
+            if(img.offsetTop < (window.innerHeight + scrollTop)) {
+              img.src = img.dataset.src;
+              img.classList.remove('lazy');
             }
         });
-    },
-    {}
-)
-for (let img of document.getElementsByTagName('img')) {
-    observer.observe(img);
-}
-
 function scale(x) {
   if (x.matches) { // If media query matches
     document.getElementById('menu-toggle').style.display = 'flex';
