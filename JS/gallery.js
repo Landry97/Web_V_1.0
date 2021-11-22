9ea0a2d09370be8abddeedc35718513133ddd556
Landry97 <92038899+Landry97@users.noreply.github.com>
2021-11-22 03:54:41 +0100

    Update gallery.js

--- JS/gallery.js
+++ JS/gallery.js
@@ -81,7 +81,7 @@ function showSlide(n) {
   dots[slideIndex - 1].style.opacity = ".7";
   dots[slideIndex - 1].style.display = "none";
   
-  dots[slideIndex - 2].style.display = "none";
+  dots[slideIndex - n].style.display = "none";
 };
 
   overlayShown = false,
