a22264a2326268817f6e3d0d0ced0ba394ff0dc0
Landry97 <92038899+Landry97@users.noreply.github.com>
2021-11-22 04:32:25 +0100

    Update gallery.js

--- JS/gallery.js
+++ JS/gallery.js
@@ -80,7 +80,7 @@ function showSlide(n) {
   dots[slideIndex - 1].style.background = "rgb(219, 10, 10)";
   dots[slideIndex - 1].style.opacity = ".7";
   };
-   for (let x = dots.length;i++){
+   for (let x = dots.length; x++) {
   dots[x].style.display = "none";
 };
 
