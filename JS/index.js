25be10200bb7f4bc42bb6c4e72203a42f075fe4f
Landry97 <92038899+Landry97@users.noreply.github.com>
2021-12-08 02:40:40 +0100

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -4,12 +4,14 @@ var info2 = document.getElementById('project-info2')
 var info3 = document.getElementById('project-info3')
 var info4 = document.getElementById('project-info4')
 var info5 = document.getElementById('project-info5')
+var info6 = document.getElementById('project-info6')
 mouseTarget.addEventListener('mouseenter', function(){ 
  info1.style.opacity = "1";
  info2.style.opacity = "1";
  info3.style.opacity = "1";
  info4.style.opacity = "1";
  info5.style.opacity = "1";
+ info6.style.opacity = "1";
  document.getElementById("bodyid").style.background = "red";
  document.getElementById("headerindex").style.color = "white";
 })
@@ -21,4 +23,5 @@ mouseTarget.addEventListener('mouseleave', function(){
  info3.style.opacity = "";
  info4.style.opacity = "";
  info5.style.opacity = "";
+ info6.style.opacity = "";
 })
